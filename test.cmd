fd2b61ded1f7c437f47c0dc21d664f4a0fa309aa
Ruslan Kuprieiev <kupruser@gmail.com>
2019-11-24 09:17:54 +0200

    Update test.cmd

--- test.cmd
+++ test.cmd
@@ -1,9 +1,16 @@
-choco install python
-refreshenv
-python --version
-python3 --version
-choco pack -v
-choco install dvc -dv -s .
-python -m dvc version
-dvc version
-choco uninstall dvc
+call choco install python || goto :error
+call refreshenv || goto :error
+call python --version || goto :error
+call python3 --version || goto :error
+call choco pack -v || goto :error
+call choco install dvc -dv -s . || goto :error
+call python -m dvc version || goto :error
+call dvc version || goto :error
+call choco uninstall dvc || goto :error
+
+echo ====== DONE ======
+goto :EOF
+
+:error
+echo ====== FAIL ======
+exit /b 1
