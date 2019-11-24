8c039a3fdfd4786bd5503a1fc8fa33f42dd86ec5
Ruslan Kuprieiev <kupruser@gmail.com>
2019-11-24 09:27:45 +0200

    Update test.cmd

--- test.cmd
+++ test.cmd
@@ -1,4 +1,4 @@
-call choco install python || goto :error
+call choco install python --version 3.7.4 || goto :error
 call refreshenv || goto :error
 call python --version || goto :error
 call choco pack -v || goto :error
