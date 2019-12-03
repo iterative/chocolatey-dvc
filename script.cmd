d8939dd85438c66dd39562fb3adca93f3be40e0b
Ruslan Kuprieiev <kupruser@gmail.com>
2019-12-03 04:06:28 +0200

    Update script.cmd

--- script.cmd
+++ script.cmd
@@ -1,4 +1,4 @@
-echo on
+@echo on
 call choco install python --version 3.7.4 || goto :error
 call refreshenv || goto :error
 call python --version || goto :error
@@ -6,8 +6,6 @@ call choco pack -v || goto :error
 call choco install dvc -dv -s . || goto :error
 call dvc version || goto :error
 call choco uninstall dvc || goto :error
-call choco apikey --key %CHOCO_API_KEY% --source https://push.chocolatey.org/ || goto :error
-for %%pkg in (dvc*.nupkg) do (call choco push %%pkg -y --force --source https://push.chocolatey.org/)
 echo ====== DONE ======
 goto :EOF
 
