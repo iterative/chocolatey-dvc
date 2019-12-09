178ec0ea64862fc983aa9961ca52508045527037
Ruslan Kuprieiev <kupruser@gmail.com>
2019-12-09 05:17:49 +0200

    Update chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -11,12 +11,17 @@ if ($proxy) {
   $env:http_proxy = $env:https_proxy = $proxy
 }
 
+Write-Host "Downloading zip"
 Get-ChocolateyWebFile -PackageName 'dvc' -Url "https://github.com/iterative/dvc/archive/$version.zip" -FileFullPath "$toolsDir\dvc-$version.zip"
 
+Write-Host "Unpacking zip"
 Get-ChocolateyUnzip -FileFullPath "$toolsDir\dvc-$version.zip" -Destination "$toolsDir"
 
+Write-Host "Changing dir"
 Set-Location -Path "dvc-$version"
 
+Write-Host "Creating build.py"
 New-Item -Path "dvc\utils" -Name "build.py" -ItemType "file" -Value "PKG = 'choco'"
 
+Write-Host "Installing from pip"
 python -m pip install .[all]
