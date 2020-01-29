33fdf2817703424d811e9492e1c4dc48a1bc2743
Ruslan Kuprieiev <kupruser@gmail.com>
2020-01-29 11:46:52 +0200

    install: update to 0.82.4

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 Update-SessionEnvironment
 
-$version = '0.80.0'
+$version = '0.82.4'
 $url = "https://github.com/iterative/dvc/archive/$version.zip"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $zipFile = "$toolsDir\dvc-$version.zip"
@@ -12,5 +12,5 @@ Set-Location -Path "$projDir"
 New-Item -Path "dvc\utils" -Name "build.py" -ItemType "file" -Value "PKG = 'choco'"
 # NOTE: not installing pyarrow, as it doesn't have wheels for Windows Server 2012,
 # see https://gist.github.com/choco-bot/d72732943836c947633625b1428a3f2c#file-install-txt-L1292
-python -m pip install '.[gs,s3,azure,oss,ssh]'
+python -m pip install '.[gs,s3,azure,oss,ssh,gdrive]'
 python -m dvc version
