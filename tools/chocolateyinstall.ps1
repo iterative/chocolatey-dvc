abfe1164755c625d3e2bd6b6bb263000078b40b1
Ruslan Kuprieiev <kupruser@gmail.com>
2020-03-03 04:45:11 +0200

    choco: update to 0.87.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 Update-SessionEnvironment
 
-$version = '0.82.9'
+$version = '0.87.0'
 $url = "https://github.com/iterative/dvc/archive/$version.zip"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $zipFile = "$toolsDir\dvc-$version.zip"
