4364c3aec23428a06ae98f972db9b56b8aff99c8
Ruslan Kuprieiev <kupruser@gmail.com>
2020-05-17 00:05:08 +0300

    install: add checksum validation

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,11 +2,12 @@ Update-SessionEnvironment
 
 $version = '0.93.0'
 $url = "https://github.com/iterative/dvc/archive/$version.zip"
+$checksum = "5a9e03d85d322929e499efb8da3f3d247c2e278f3a75948caf40b56b9209a44d"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $zipFile = "$toolsDir\dvc-$version.zip"
 $projDir = "$toolsDir\dvc-$version"
 
-Get-ChocolateyWebFile -PackageName 'dvc' -Url "$url" -FileFullPath "$zipFile"
+Get-ChocolateyWebFile -PackageName 'dvc' -Url "$url" -FileFullPath "$zipFile" -ChecksumType sha256 -Checksum "$checksum"
 Get-ChocolateyUnzip -FileFullPath "$zipFile" -Destination "$toolsDir"
 Set-Location -Path "$projDir"
 New-Item -Path "dvc\utils" -Name "build.py" -ItemType "file" -Value "PKG = 'choco'"
