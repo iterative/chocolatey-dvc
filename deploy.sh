066b91943e062d5b0311e076a133e8fbb6e033cf
Ruslan Kuprieiev <kupruser@gmail.com>
2020-10-29 14:42:40 +0200

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-set -e
-set -x
-
-choco apikey --key $CHOCO_API_KEY --source https://push.chocolatey.org/
-#push might return non-zero return code if we are re-pushing existing package version
-choco push dvc*.nupkg -y --force --source https://push.chocolatey.org/ || true
