cd18e950dbeb0c180c004e7faf25e22be6afb775
Ruslan Kuprieiev <kupruser@gmail.com>
2020-10-29 14:00:16 +0200

    build: don't install python

--- build.cmd
+++ build.cmd
@@ -1,7 +1,4 @@
 @echo on
-call choco install python --version 3.7.4 || goto :error
-call refreshenv || goto :error
-call python --version || goto :error
 call choco pack -v || goto :error
 call choco install dvc -dv -s . || goto :error
 call dvc version || goto :error
