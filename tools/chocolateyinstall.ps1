dbb30964545577a73b4f14432c3f41ddeb2f82f0
Ruslan Kuprieiev <kupruser@gmail.com>
2022-06-10 12:24:44 +0300

    install: use archives from pypi (#85)

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 Update-SessionEnvironment
 
 $version = '2.11.0'
-$url = "https://github.com/iterative/dvc/archive/$version.zip"
-$checksum = '1bbd6f36ae10bfcd97c5199a25899546027ebddb8821896064cfb75817181ce0'
+$url = "https://pypi.io/packages/source/d/dvc/dvc-$version.tar.gz"
+$checksum = '26f5ba2a89a94874a4cc5046835717f8122c6e9adcf7097f5ec93a3a816388a0'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$zipFile = "$toolsDir\dvc-$version.zip"
+$tarFile = "$toolsDir\dvc-$version.tar.gz"
 $projDir = "$toolsDir\dvc-$version"
 
-Get-ChocolateyWebFile -PackageName 'dvc' -Url "$url" -FileFullPath "$zipFile" -ChecksumType sha256 -Checksum "$checksum"
-Get-ChocolateyUnzip -FileFullPath "$zipFile" -Destination "$toolsDir"
+Get-ChocolateyWebFile -PackageName 'dvc' -Url "$url" -FileFullPath "$tarFile" -ChecksumType sha256 -Checksum "$checksum"
+Get-ChocolateyUnzip -FileFullPath "$tarFile" -Destination "$toolsDir"
 Set-Location -Path "$projDir"
 New-Item -Path "dvc\utils" -Name "build.py" -ItemType "file" -Value "PKG = 'choco'"
 python -m pip install --upgrade pip
