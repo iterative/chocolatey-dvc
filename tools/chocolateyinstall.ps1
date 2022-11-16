8e6a7fa05b45cdfb573a46c4ab7bfae0661f2827
efiop <efiop@users.noreply.github.com>
2022-11-16 00:26:22 +0000

    dvc 2.34.2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 Update-SessionEnvironment
 
-$version = '2.34.1'
+$version = '2.34.2'
 $url = "https://pypi.io/packages/source/d/dvc/dvc-$version.tar.gz"
-$checksum = '4919f1e458fe99c03862e18f60204cc27b8347e70403efbc57b3f90cc1011a64'
+$checksum = 'a995dff74bfa562ad772c55d1856b5affe33850d16d33a1449d8779b3ed86b67'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $tarFile = "$toolsDir\dvc-$version.tar.gz"
 $projDir = "$toolsDir\dvc-$version"
