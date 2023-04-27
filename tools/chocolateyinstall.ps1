9a4e213c9b3b7b3d21ff5398dd53622da304b8fa
Ruslan Kuprieiev <kupruser@gmail.com>
2023-04-27 13:32:05 +0300

    Update chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -8,10 +8,18 @@ $url = "https://pypi.io/packages/source/d/dvc/dvc-$version.tar.gz"
 $checksum = '3ee239ae1e946fe1ac8999fc829ccb570e03c248938e5b8235f5647cf1dce806'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $tarFile = "$toolsDir\dvc-$version.tar.gz"
+$targzFile = "$tarFile.gz"
 $projDir = "$toolsDir\dvc-$version"
 
-Get-ChocolateyWebFile -PackageName 'dvc' -Url "$url" -FileFullPath "$tarFile" -ChecksumType sha256 -Checksum "$checksum"
-Get-ChocolateyUnzip -FileFullPath "$tarFile" -Destination "$toolsDir"
+Get-ChocolateyWebFile -PackageName 'dvc' -Url "$url" -FileFullPath "$targzFile" -ChecksumType sha256 -Checksum "$checksum"
+Get-ChocolateyUnzip -FileFullPath "$targzFile" -Destination "$toolsDir"
+if (Test-Path "$tarFile") {
+  # workaround for https://github.com/chocolatey/choco/pull/1026
+  # inspired by https://github.com/chocolatey-community/chocolatey-packages/blob/d00d111cdc6e638146359073417f9c1d57f8da84/automatic/kubernetes-cli/tools/chocolateyInstall.ps1#L15
+  Get-ChocolateyUnzip -FileFullPath "$tarFile" -Destination "$toolsDir"
+  Remove-Item "$tarFile"
+}
+
 Get-ChildItem -Path "$toolsDir" -Depth 2
 Set-Location -Path "$projDir"
 New-Item -Path "dvc\utils" -Name "build.py" -ItemType "file" -Value "PKG = 'choco'"
